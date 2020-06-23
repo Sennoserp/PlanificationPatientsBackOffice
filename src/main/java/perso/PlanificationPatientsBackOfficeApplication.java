3130084904b930cf52fe167fa520eb73a554dc03
Flora Dupont <flora.dupont@hotmail.fr>
2020-06-23 12:41:59 +0200

    first commit projet

---
+++ src/main/java/perso/PlanificationPatientsBackOfficeApplication.java
@@ -0,0 +1,13 @@
+package perso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanificationPatientsBackOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanificationPatientsBackOfficeApplication.class, args);
+	}
+
+}
